93a572a8014b9527a9e093ee97f278c0d2b0fb63
rockfeng0 <lkf20031988@163.com>
2018-03-27 10:45:28 +0800

    init

---
+++ t.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+'''
+Current module: t
+
+Rough version history:
+v1.0    Original version to use
+
+********************************************************************
+    @AUTHOR:  Administrator-Bruce Luo(罗科峰)
+    MAIL:    lkf20031988@163.com
+    RCS:      t,v 1.0 2018年3月27日
+    FROM:   2018年3月27日
+********************************************************************
+
+======================================================================
+
+UI and Web Http automation frame for python.
+
+'''
+
